23b5ba70151cb15bffebc3c854989817e9a8aaae
Pascal Obry <pascal@obry.net>
2017-11-03 18:21:45 +0100

    Revert "Try to avoid multiple call to onResume()."
    The ground testing has shown that this does not fix the issue.
    
    This reverts commit aa56ef5bd12dd99e02356a2c481c681d6f0647ae.

--- src/Main.java
+++ src/Main.java
@@ -55,7 +55,6 @@ public class Main extends android.app.Activity
     ViewGroup PickerExtra, SaveAsExtra;
     boolean ShuttingDown = false;
     boolean StateLoaded = false; /* will be reset to false every time activity is killed and restarted */
-    boolean ResumeBlock = false;
 
     static final int NotifyProgramDone = 1; /* arbitrary notification ID */
 
@@ -1492,13 +1491,6 @@ public class Main extends android.app.Activity
       {
         super.onResume();
         Notiman.cancel(NotifyProgramDone);
-
-        /*  prevent mltiple onResume(), this can happen and is not something we handle here */
-        if (ResumeBlock)
-            return;
-        else
-            ResumeBlock = true;
-
         if (!StateLoaded)
           {
             Global.StartBGTask
