4429752ddecc1a3abfd4a092b9186818f7d73665
Pascal Obry <pascal@obry.net>
2024-04-16 20:50:00 +0200

    se-structural-engineering: New module (generated from ROM).

--- app/src/main/java/net/obry/ti5x/Main.java
+++ app/src/main/java/net/obry/ti5x/Main.java
@@ -96,6 +96,7 @@ public class Main extends AppCompatActivity {
         new BuiltinLibrary(R.string.mathutil_library, R.raw.mu),
         new BuiltinLibrary(R.string.electrical_library, R.raw.ee),
         new BuiltinLibrary(R.string.rpn_library, R.raw.rp),
+        new BuiltinLibrary(R.string.structuralengineering_library, R.raw.se),
         new BuiltinLibrary(R.string.civil_engineering_library, R.raw.ce),
         new BuiltinLibrary(R.string.contribution_library, R.raw.ct)
      };
